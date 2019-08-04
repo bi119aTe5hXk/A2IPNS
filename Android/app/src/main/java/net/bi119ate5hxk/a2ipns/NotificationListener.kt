1c72cfda69b42e9659c12e17b999c99ee252bd35
xlfdll <xlfdll@gmail.com>
2019-08-03 22:41:46 -0700

    Added code to generate Apple Push Notification JSON

--- Android/app/src/main/java/net/bi119ate5hxk/a2ipns/NotificationListener.kt
+++ Android/app/src/main/java/net/bi119ate5hxk/a2ipns/NotificationListener.kt
@@ -1,10 +1,9 @@
 package net.bi119ate5hxk.a2ipns
 
-import android.app.Notification
 import android.content.Intent
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import org.json.JSONObject
 
 class NotificationListener : NotificationListenerService() {
     override fun onNotificationPosted(sbn: StatusBarNotification?) {
@@ -13,6 +12,9 @@ class NotificationListener : NotificationListenerService() {
             sbn?.notification?.extras?.getString("android.text") ?: "",
             sbn?.packageName ?: "<Unknown>"
         )
+
+        val json = generateAppleJson(item)
+
         val intent = Intent("net.bi119ate5hxk.a2ipns.NOTIFICATION_SERVICE")
 
         intent.putExtra("notification_item", item)
@@ -20,7 +22,23 @@ class NotificationListener : NotificationListenerService() {
         sendBroadcast(intent)
     }
 
-    override fun onNotificationRemoved(sbn: StatusBarNotification?) {
-        super.onNotificationRemoved(sbn)
+    private fun generateAppleJson(item: NotificationItem): String {
+        val rootJsonObject = JSONObject()
+
+        rootJsonObject.put("aps", JSONObject())
+
+        val apsJsonObject = rootJsonObject.getJSONObject("aps")
+
+        apsJsonObject.put("alert", JSONObject())
+
+        val alertJsonObject = apsJsonObject.getJSONObject("alert")
+
+        alertJsonObject.put("title", item.Title)
+        alertJsonObject.put("subtitle", item.Source)
+        alertJsonObject.put("body", item.Text)
+
+        apsJsonObject.put("sound", "default")
+
+        return rootJsonObject.toString()
     }
 }
