abf3af987b4578a437008d44e2bb6e3c69038917
bi119aTe5hXk <billgateshxk@gmail.com>
2019-08-04 16:14:39 +0900

    change key from demo

--- Web/cert.php
+++ Web/cert.php
@@ -1,5 +1,5 @@
 <?php
-    $key = pack('H*', "bcb04b7e103a0cd8b54763051cef08bc55abe029fdebae5e1d417e2ffb2a00a3");
+    $key = pack('H*', "123456789");
     $plaintext = "123456789";
 
     $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
