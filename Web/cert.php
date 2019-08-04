7969653cf3fa0b1fe65f275202f07ea191e9f0b4
bi119aTe5hXk <billgateshxk@gmail.com>
2019-08-04 16:13:22 +0900

    php encrypt

---
+++ Web/cert.php
@@ -0,0 +1,19 @@
+<?php
+    $key = pack('H*', "bcb04b7e103a0cd8b54763051cef08bc55abe029fdebae5e1d417e2ffb2a00a3");
+    $plaintext = "123456789";
+
+    $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+    $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+    
+    $ciphertext = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key,  $plaintext, MCRYPT_MODE_CBC, $iv);
+    $ciphertext = $iv . $ciphertext;
+    $ciphertext_base64 = base64_encode($ciphertext);
+
+    $array = array(
+        "time" => "20190804" ,
+        "cert" => $ciphertext_base64,
+    );
+    $json = json_encode( $array );
+    echo $json;
+    //echo  $ciphertext_base64 . "\n";
+?>
