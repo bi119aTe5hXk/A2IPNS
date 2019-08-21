c0b92348bc56f777ca835245c43738a7267845c9
xlfdll <xlfdll@gmail.com>
2019-08-21 02:30:47 -0700

    Added code to show device token pairing prompt during first-run

--- Android/app/src/main/java/org/xlfdll/a2pns/MainActivity.kt
+++ Android/app/src/main/java/org/xlfdll/a2pns/MainActivity.kt
@@ -32,14 +32,28 @@ class MainActivity : AppCompatActivity() {
         // Has to set title here, as manifest's label attributes do not work consistently
         this.title = getString(R.string.app_title)
 
+        if (!AppHelper.isLaunched) {
+            // App settings
+            AppHelper.init(applicationContext)
+
+            // Update APNS authentication token
+            ViewHelper.updateAPNSAuthToken(this)
+
+            AppHelper.isLaunched = true
+        }
+
         // Notification listener and receiver initialization
         if (!isNotificationListenerEnabled(applicationContext)) {
             openNotificationListenerSettings()
         }
 
+        showDeviceTokenPrompt()
+
         val filter = IntentFilter("org.xlfdll.a2pns.NOTIFICATION_SERVICE")
 
         registerReceiver(receiver, filter)
+
+        initNotificationList()
 
         enableSwitch.setOnCheckedChangeListener { _, isChecked ->
             if (!isNotificationListenerEnabled(applicationContext)) {
@@ -51,16 +65,7 @@ class MainActivity : AppCompatActivity() {
             ) {
                 enableSwitch.isChecked = false
 
-                val builder = AlertDialog.Builder(this)
-
-                builder.setTitle(R.string.alert_no_device_token_title)
-                    .setMessage(R.string.alert_no_device_token_message)
-                    .setPositiveButton(R.string.alert_button_yes, DialogInterface.OnClickListener { _, _ ->
-                        startActivity(Intent(this, QRCodeActivity::class.java))
-                    })
-                    .setNegativeButton(R.string.alert_button_no, null)
-                    .create()
-                    .show()
+                showDeviceTokenPrompt()
             } else {
                 val prefEditor = AppHelper.Settings.edit()
 
@@ -75,19 +80,8 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        if (!AppHelper.isLaunched) {
-            // App settings
-            AppHelper.init(applicationContext)
-
-            // Update APNS authentication token
-            ViewHelper.updateAPNSAuthToken(this)
-
-            AppHelper.isLaunched = true
-        }
-
-        initNotificationList()
-
-        enableSwitch.isChecked = AppHelper.Settings.getBoolean(getString(R.string.pref_key_enable_service), false)
+        enableSwitch.isChecked =
+            AppHelper.Settings.getBoolean(getString(R.string.pref_key_enable_service), false)
     }
 
     override fun onDestroy() {
@@ -116,7 +110,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun isNotificationListenerEnabled(context: Context): Boolean {
-        return NotificationManagerCompat.getEnabledListenerPackages(this).contains(context.packageName)
+        return NotificationManagerCompat.getEnabledListenerPackages(this)
+            .contains(context.packageName)
     }
 
     private fun openNotificationListenerSettings() {
@@ -124,6 +119,27 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS))
         } else {
             startActivity(Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"))
+        }
+    }
+
+    private fun showDeviceTokenPrompt() {
+        if (AppHelper.Settings.getString(
+                getString(R.string.pref_key_device_token),
+                null
+            ) == null
+        ) {
+            val builder = AlertDialog.Builder(this)
+
+            builder.setTitle(R.string.alert_no_device_token_title)
+                .setMessage(R.string.alert_no_device_token_message)
+                .setPositiveButton(
+                    R.string.alert_button_yes,
+                    DialogInterface.OnClickListener { _, _ ->
+                        startActivity(Intent(this, QRCodeActivity::class.java))
+                    })
+                .setNegativeButton(R.string.alert_button_no, null)
+                .create()
+                .show()
         }
     }
 
@@ -146,7 +162,8 @@ class MainActivity : AppCompatActivity() {
 
             notificationChannel.description = getString(R.string.notification_channel_description)
 
-            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
             notificationManager.createNotificationChannel(notificationChannel)
         }
@@ -162,7 +179,8 @@ class MainActivity : AppCompatActivity() {
             .setContentIntent(pendingIntent)
             .build()
 
-        notification.flags = notification.flags or Notification.FLAG_NO_CLEAR or Notification.FLAG_ONGOING_EVENT
+        notification.flags =
+            notification.flags or Notification.FLAG_NO_CLEAR or Notification.FLAG_ONGOING_EVENT
 
         val notifier = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
@@ -170,7 +188,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun hideNotificationIcon() {
-        val notifier = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notifier =
+            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         notifier.cancel(AppHelper.NOTIFICATION_ID)
     }
